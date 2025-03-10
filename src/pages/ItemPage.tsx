3fd28357e1ffbb87405b1c3704258878c32d9c80
Ouiki <39866518+VioleGracex@users.noreply.github.com>
2025-03-10 18:55:51 +0500

    Update ItemPage.tsx

--- src/pages/ItemPage.tsx
+++ src/pages/ItemPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import AdDetail from '../components/AdDetail';
+
 
 const ItemPage: React.FC = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Item Page</h1>
-      <AdDetail />
+     
     </div>
   );
 };
